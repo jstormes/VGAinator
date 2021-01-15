b4603d65b8741a053aa34c39d3747da358940c90
James Stormes <james.s@digitalroominc.com>
2021-01-14 22:23:11 -0600

    Updating Testing2 Schmatic

---
+++ cmake-build-debug/CMakeFiles/Testing2_640x480at60_25.175MHz.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Testing2_640x480at60_25.175MHz.dir/Testing2_640x480at60_25.175MHz.c.o"
+  "Testing2_640x480at60_25.175MHz"
+  "Testing2_640x480at60_25.175MHz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Testing2_640x480at60_25.175MHz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
